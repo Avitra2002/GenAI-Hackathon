8b5a9489f86681719c479fa2b297a42270deb0ba
Sze Lee <szelee@temasek.com.sg>
2024-02-17 20:44:31 +0800

    Refactor completion api params

--- utils/llm.py
+++ utils/llm.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from dotenv import load_dotenv
 from loguru import logger
@@ -18,6 +18,8 @@ DEFAULT_TEMPERATURE = 0.0
 DEFAULT_TOP_P = 0.95
 DEFAULT_FREQUENCY_PENALTY = 0.0
 DEFAULT_PRESENCE_PENALTY = 0.0
+DEFAULT_N_PAST_MESSAGES = 11
+DEFAULT_SEED = None
 
 LOG_FILEPATH = "./logs/out.log"
 load_dotenv()
@@ -29,24 +31,31 @@ if os.environ.get("DISABLE_LOG_TO_TERMINAL").lower() == "true":
 logger.add(LOG_FILEPATH)
 
 
+# References
+# https://cookbook.openai.com/examples/how_to_format_inputs_to_chatgpt_models#2-an-example-chat-completion-api-call
+# https://cookbook.openai.com/examples/how_to_stream_completions
+# https://platform.openai.com/docs/guides/text-generation/how-should-i-set-the-temperature-parameter
+# https://platform.openai.com/docs/guides/text-generation/frequency-and-presence-penalties
+# https://platform.openai.com/docs/guides/text-generation/reproducible-outputs
 def get_completion(
     messages: List[Dict[str, str]],
-    max_tokens: int = DEFAULT_MAX_TOKENS,
-    temperature: float = DEFAULT_TEMPERATURE,
-    top_p: float = DEFAULT_TOP_P,
-    frequency_penalty: float = DEFAULT_FREQUENCY_PENALTY,
-    presence_penalty: float = DEFAULT_PRESENCE_PENALTY,
+    max_tokens: Optional[int] = DEFAULT_MAX_TOKENS,
+    temperature: Optional[float] = DEFAULT_TEMPERATURE,
+    top_p: Optional[float] = DEFAULT_TOP_P,
+    frequency_penalty: Optional[float] = DEFAULT_FREQUENCY_PENALTY,
+    presence_penalty: Optional[float] = DEFAULT_PRESENCE_PENALTY,
+    seed: Optional[int] = DEFAULT_SEED,
 ):
     response = client.chat.completions.create(
         model=DEFAULT_MODEL,
         messages=messages,
-        stream=False,  # https://cookbook.openai.com/examples/how_to_stream_completions
-        max_tokens=max_tokens,  # max token for each response
-        temperature=temperature,  # https://platform.openai.com/docs/guides/text-generation/how-should-i-set-the-temperature-parameter
+        max_tokens=max_tokens,
+        temperature=temperature,
         top_p=top_p,
-        frequency_penalty=frequency_penalty,  # https://platform.openai.com/docs/guides/text-generation/frequency-and-presence-penalties
+        frequency_penalty=frequency_penalty,
         presence_penalty=presence_penalty,
-        seed=42,  # https://platform.openai.com/docs/guides/text-generation/reproducible-outputs
+        seed=seed,
+        stream=False,
     )
     log_usage(prompt=messages, completion=response.choices[0].message.content)
     return response
@@ -56,11 +65,12 @@ class CompletionStream:
     def __init__(
         self,
         messages: List[Dict[str, str]],
-        max_tokens: int = DEFAULT_MAX_TOKENS,
-        temperature: float = DEFAULT_TEMPERATURE,
-        top_p: float = DEFAULT_TOP_P,
-        frequency_penalty: float = DEFAULT_FREQUENCY_PENALTY,
-        presence_penalty: float = DEFAULT_PRESENCE_PENALTY,
+        max_tokens: Optional[int] = DEFAULT_MAX_TOKENS,
+        temperature: Optional[float] = DEFAULT_TEMPERATURE,
+        top_p: Optional[float] = DEFAULT_TOP_P,
+        frequency_penalty: Optional[float] = DEFAULT_FREQUENCY_PENALTY,
+        presence_penalty: Optional[float] = DEFAULT_PRESENCE_PENALTY,
+        seed: Optional[int] = DEFAULT_SEED,
     ):
         self.messages = messages
         self.completion = None
@@ -69,18 +79,19 @@ class CompletionStream:
         self.top_p = top_p
         self.frequency_penalty = frequency_penalty
         self.presence_penalty = presence_penalty
+        self.seed = seed
 
     def __enter__(self):
         response = client.chat.completions.create(
             model=DEFAULT_MODEL,
             messages=self.messages,
-            stream=True,  # https://cookbook.openai.com/examples/how_to_stream_completions
-            max_tokens=self.max_tokens,  # max token for each response
-            temperature=self.temperature,  # https://platform.openai.com/docs/guides/text-generation/how-should-i-set-the-temperature-parameter
+            max_tokens=self.max_tokens,
+            temperature=self.temperature,
             top_p=self.top_p,
-            frequency_penalty=self.frequency_penalty,  # https://platform.openai.com/docs/guides/text-generation/frequency-and-presence-penalties
+            frequency_penalty=self.frequency_penalty,
             presence_penalty=self.presence_penalty,
-            seed=42,  # https://platform.openai.com/docs/guides/text-generation/reproducible-outputs
+            seed=self.seed,
+            stream=True,
         )
         return response
 
