d82e4d3ad859150ac3d3bdf063e7f64db4ec2d46
Sze Lee <szelee@temasek.com.sg>
2024-02-25 17:15:53 +0800

    Cleanup

--- hackathon.py
+++ hackathon.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from qna import load_qna
 
+from qna import load_qna
 
 st.set_page_config(page_title="GenAI Hackathon 2024", page_icon="🤖", layout="centered")
 st.image("poster.png")
