a11ea6c82243e0c3a4b2e0b7ad76553a8ba8599a
Sze Lee <szelee@temasek.com.sg>
2024-03-02 16:11:46 +0800

    Add streaming option

--- pages/01_❓_basic_qna.py
+++ pages/01_❓_basic_qna.py
@@ -1,9 +1,16 @@
 import streamlit as st
 
-from utils.llm import get_completion
+from utils.llm import get_completion, CompletionStream
+
+is_streaming = st.sidebar.checkbox("Stream reply?")
 
 if user_input := st.text_input("Question: "):
     messages = [{"role": "user", "content": user_input}]
 
-    response = get_completion(messages)
-    st.write(f"Answer: {response.choices[0].message.content}")
+    if is_streaming:
+        stream = CompletionStream(messages)
+        with stream as response:
+            stream.completion = st.write_stream(response)
+    else:
+        response = get_completion(messages)
+        st.write(f"Answer: {response.choices[0].message.content}")
